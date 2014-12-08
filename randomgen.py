68ea12957b646208f4e1386640aa47977fac1251
Katherine Crowson <crowsonkb@gmail.com>
2014-12-08 12:34:20 -0800

    Refactor + print entropy to stderr

--- randomgen.py
+++ randomgen.py
@@ -3,8 +3,12 @@
 import argparse
 import math
 import random
+import sys
 
 ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def bold(s):
+    return '\x1b[1m' + s + '\x1b[0m'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,8 +20,9 @@ def main():
     args = parser.parse_args()
     rng = random.SystemRandom()
 
-    print('\x1b[1mentropy: {:.1f} bits\x1b[0m'.format(
-        math.log(len(ALPHABET), 2) * args.length))
+    print(bold('entropy: {:.1f} bits').format(
+        math.log(len(ALPHABET), 2) * args.length), file=sys.stderr)
+
     for i in range(args.count):
         s = ''
         for j in range(args.length):
