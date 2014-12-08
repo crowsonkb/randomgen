e8cbcd7a622dd986af365dd480d78398d9910703
Katherine Crowson <crowsonkb@gmail.com>
2014-12-08 12:29:34 -0800

    pylint nitpicking

--- randomgen.py
+++ randomgen.py
@@ -4,21 +4,25 @@ import argparse
 import math
 import random
 
-alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--count', help='number of passwords generated',
-    default=5, type=int)
-parser.add_argument('-l', '--length', help='length of passwords generated',
-    default=16, type=int)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--count', help='number of passwords generated',
+                        default=5, type=int)
+    parser.add_argument('-l', '--length', help='length of passwords generated',
+                        default=16, type=int)
 
-args = parser.parse_args()
-rng = random.SystemRandom()
+    args = parser.parse_args()
+    rng = random.SystemRandom()
 
-print('\x1b[1mentropy: {:.1f} bits\x1b[0m'.format(
-	math.log(len(alphabet), 2) * args.length))
-for i in range(args.count):
-    s = ''
-    for j in range(args.length):
-        s += rng.choice(alphabet)
-    print(s)
+    print('\x1b[1mentropy: {:.1f} bits\x1b[0m'.format(
+        math.log(len(ALPHABET), 2) * args.length))
+    for i in range(args.count):
+        s = ''
+        for j in range(args.length):
+            s += rng.choice(ALPHABET)
+        print(s)
+
+if __name__ == "__main__":
+    main()
