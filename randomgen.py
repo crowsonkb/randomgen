eb19bf16b1f22535513b050553e6cc8b98f6db8b
Katherine Crowson <crowsonkb@gmail.com>
2014-12-11 23:35:50 -0800

    Refactoring

--- randomgen.py
+++ randomgen.py
@@ -9,8 +9,8 @@ import sys
 
 ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-def bold(s):
-    return '\x1b[1m' + s + '\x1b[0m'
+def bold(string):
+    return '\x1b[1m' + string + '\x1b[0m'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,18 +18,15 @@ def main():
                         default=5, type=int)
     parser.add_argument('-l', '--length', help='length of passwords generated',
                         default=16, type=int)
-
     args = parser.parse_args()
+
+    entropy = math.log(len(ALPHABET), 2) * args.length
+    print(bold('entropy: {:.1f} bits'.format(entropy)), file=sys.stderr)
+
     rng = random.SystemRandom()
-
-    print(bold('entropy: {:.1f} bits').format(
-        math.log(len(ALPHABET), 2) * args.length), file=sys.stderr)
-
     for _ in range(args.count):
-        s = ''
-        for _ in range(args.length):
-            s += rng.choice(ALPHABET)
-        print(s)
+        chars = [rng.choice(ALPHABET) for _ in range(0, args.length)]
+        print(''.join(chars))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
